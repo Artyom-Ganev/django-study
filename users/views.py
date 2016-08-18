3efdd5bfcc124ef3c9d739bb3f94abfc54cf838e
Artyom Ganev <a.a.ganev@gmail.com>
2016-08-18 18:03:51 +0300

    add users app & registration form

---
+++ users/views.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.views.generic.edit import FormView
+
+
+class RegisterFormView(FormView):
+    form_class = UserCreationForm
+
+    success_url = "/login/"
+
+    template_name = "register.html"
+
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterFormView, self).form_valid(form)
+
+
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+    template_name = "login.html"
+    success_url = "/"
+
+    def form_valid(self, form):
+        self.user = form.get_user()
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
