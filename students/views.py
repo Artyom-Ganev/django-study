318400ce865bb9379d602a65cd2e72b9ac5316ce
Artyom Ganev <a.a.ganev@gmail.com>
2016-08-18 12:34:51 +0300

    views & templates small refactoring

--- students/views.py
+++ students/views.py
@@ -1,9 +1,24 @@
 # coding: utf-8
+from django.http import Http404
 from django.http import HttpResponse
 from django.template import Context
 from django.template import loader
 
 from students.models import Student, Group, Speciality, Faculty, Department
+
+html = ".html"
+
+
+def get_template(objects, name):
+    template = loader.get_template(name + html)
+    context = Context({name: objects})
+    return template.render(context)
+
+
+def get_template_with_detail(objects, name, detail, detail_name):
+    template = loader.get_template(name + html)
+    context = Context({name: objects, detail_name: detail})
+    return template.render(context)
 
 
 def index(request):
@@ -13,69 +28,68 @@ def index(request):
 
 def faculties(request):
     objects = Faculty.objects.all()
-    template = loader.get_template("faculties.html")
-    context = Context({"faculties": objects})
-    return HttpResponse(template.render(context))
+    return HttpResponse(get_template(objects, "faculties"))
 
 
 def faculty(request, fac_id):
-    obj = Faculty.objects.get(id=fac_id)
-    template = loader.get_template("faculty.html")
-    context = Context({"faculty": obj})
-    return HttpResponse(template.render(context))
+    try:
+        obj = Faculty.objects.get(id=fac_id)
+    except Faculty.DoesNotExist:
+        raise Http404
+    detail = Department.objects.filter(faculty=fac_id)
+    return HttpResponse(get_template_with_detail(obj, "faculty", detail, "departments"))
 
 
 def departments(request):
     objects = Department.objects.all()
-    template = loader.get_template("departments.html")
-    context = Context({"departments": objects})
-    return HttpResponse(template.render(context))
+    return HttpResponse(get_template(objects, "departments"))
 
 
 def department(request, dep_id):
-    obj = Department.objects.get(id=dep_id)
-    template = loader.get_template("department.html")
-    context = Context({"department": obj})
-    return HttpResponse(template.render(context))
+    try:
+        obj = Department.objects.get(id=dep_id)
+    except Department.DoesNotExist:
+        raise Http404
+    detail = Speciality.objects.filter(department=dep_id)
+    return HttpResponse(get_template_with_detail(obj, "department", detail, "specialities"))
 
 
 def specialities(request):
     objects = Speciality.objects.all()
-    template = loader.get_template("specialities.html")
-    context = Context({"specialities": objects})
-    return HttpResponse(template.render(context))
+    return HttpResponse(get_template(objects, "specialities"))
 
 
 def specialitiy(request, spec_id):
-    obj = Speciality.objects.get(id=spec_id)
-    template = loader.get_template("speciality.html")
-    context = Context({"speciality": obj})
-    return HttpResponse(template.render(context))
+    try:
+        obj = Speciality.objects.get(id=spec_id)
+    except Speciality.DoesNotExist:
+        raise Http404
+    detail = Group.objects.filter(speciality=spec_id)
+    return HttpResponse(get_template_with_detail(obj, "speciality", detail, "groups"))
 
 
 def groups(request):
     objects = Group.objects.all()
-    template = loader.get_template("groups.html")
-    context = Context({"groups": objects})
-    return HttpResponse(template.render(context))
+    return HttpResponse(get_template(objects, "groups"))
 
 
 def group(request, group_id):
-    obj = Group.objects.get(id=group_id)
-    template = loader.get_template("group.html")
-    context = Context({"group": obj})
-    return HttpResponse(template.render(context))
+    try:
+        obj = Group.objects.get(id=group_id)
+    except Group.DoesNotExist:
+        raise Http404
+    detail = Student.objects.filter(group=obj)
+    return HttpResponse(get_template_with_detail(obj, "group", detail, "students"))
 
 
 def students(request):
     objects = Student.objects.all()
-    template = loader.get_template("students.html")
-    context = Context({"students": objects})
-    return HttpResponse(template.render(context))
+    return HttpResponse(get_template(objects, "students"))
 
 
 def student(request, student_number):
-    obj = Student.objects.get(number=student_number)
-    template = loader.get_template("student.html")
-    context = Context({"student": obj})
-    return HttpResponse(template.render(context))
+    try:
+        obj = Student.objects.get(number=student_number)
+    except Student.DoesNotExist:
+        raise Http404
+    return HttpResponse(get_template(obj, "student"))
